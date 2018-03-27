d585431aa662af2c83ddeca28f66fc2628e676fc
Sean Porter <portertech@gmail.com>
2018-03-26 20:42:18 -0700

    fixed cli specs
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- spec/sensu/translator/cli_spec.rb
+++ spec/sensu/translator/cli_spec.rb
@@ -8,7 +8,9 @@ describe "Sensu::Translator::CLI" do
     expected = {
       :config_file => "/etc/sensu/config.json",
       :config_dirs => ["/etc/sensu/conf.d"],
-      :output_file => "/tmp/sensu_translated.json"
+      :environment => "default",
+      :organization => "default",
+      :output_dir => "/tmp/sensu_v2"
     }
     expect(options).to eq(expected)
   end
@@ -17,12 +19,16 @@ describe "Sensu::Translator::CLI" do
     options = Sensu::Translator::CLI.read([
       "-c", "spec/config.json",
       "-d", "spec/conf.d",
-      "-o", "spec/translated.json",
+      "-o", "spec/sensu_v2",
+      "-O", "acme",
+      "-E", "spec"
     ])
     expected = {
       :config_file => "spec/config.json",
       :config_dirs => ["spec/conf.d"],
-      :output_file => "spec/translated.json"
+      :output_dir => "spec/sensu_v2",
+      :organization => "acme",
+      :environment => "spec"
     }
     expect(options).to eq(expected)
   end
